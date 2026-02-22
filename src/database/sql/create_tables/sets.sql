3dfa0e5ad1320441d3eb6b6240fd6fca322d1016
BendRJ <j.h.b@mailbox.org>
2026-02-22 17:46:29 +0100

    feat(database): add new card schemas for instants, lands, planeswalkers, and sorceries; update SQL tables and upsert scripts

--- src/database/sql/create_tables/sets.sql
+++ src/database/sql/create_tables/sets.sql
@@ -1,7 +1,11 @@
 CREATE TABLE IF NOT EXISTS sets (
     code TEXT PRIMARY KEY,
     name TEXT NOT NULL,
-    type TEXT,
-    release_date DATE,
-    online_only BOOLEAN NOT NULL DEFAULT FALSE
+    set_type TEXT,
+    released_at DATE,
+    card_count INTEGER,
+    digital BOOLEAN NOT NULL DEFAULT FALSE,
+    foil_only BOOLEAN NOT NULL DEFAULT FALSE,
+    nonfoil_only BOOLEAN NOT NULL DEFAULT FALSE,
+    icon_svg_uri TEXT
 );
