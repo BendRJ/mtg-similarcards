eb5b5f1e6f051beb804a1f7309da7592eca33dbf
BendRJ <j.h.b@mailbox.org>
2026-03-08 15:06:10 +0100

    chore: move example upsert script to scripts folder

---
+++ scripts/example_upsert.py
@@ -0,0 +1,40 @@
+"""
+Example upsert for the sets table using Tarkir: Dragonstorm data.
+"""
+from pathlib import Path
+from typing import LiteralString, cast
+
+from database.db import get_cursor
+
+# Load SQL statement from file
+SQL_FILE = Path(__file__).parent.parent / "src" / "database" / "sql" / "upsert" / "sets_upsert.sql"
+SETS_UPSERT_SQL = cast(LiteralString, SQL_FILE.read_text())
+
+# Set identification
+SET_CODE = "tdm"
+SET_NAME = "Tarkir: Dragonstorm"
+
+# Set properties
+SET_TYPE = "expansion"
+RELEASED_AT = "2025-04-11"
+CARD_COUNT = 427
+SEARCH_URI = "https://api.scryfall.com/cards/search?order=set&q=e%3Atdm&unique=prints"
+DIGITAL = False
+FOIL_ONLY = False
+NONFOIL_ONLY = False
+ICON_SVG_URI = "https://svgs.scryfall.io/sets/tdm.svg?1771218000"
+
+with get_cursor() as cur:
+    cur.execute(SETS_UPSERT_SQL, (
+        SET_CODE,
+        SET_NAME,
+        SET_TYPE,
+        RELEASED_AT,
+        CARD_COUNT,
+        SEARCH_URI,
+        DIGITAL,
+        FOIL_ONLY,
+        NONFOIL_ONLY,
+        ICON_SVG_URI
+    ))
+    print(f"Inserted/Updated set {SET_NAME} ({SET_CODE})")
