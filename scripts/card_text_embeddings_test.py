0eb90eef3360545fd20f4d022a5f33972270ed6f
BendRJ <j.h.b@mailbox.org>
2026-07-05 20:07:14 +0200

    refactor: update schema file path to use Path for improved readability and maintainability

--- scripts/card_text_embeddings_test.py
+++ scripts/card_text_embeddings_test.py
@@ -1,7 +1,10 @@
+from pathlib import Path
 from app.services.vector_service import build_card_text
 import json
 
-with open("src/database/schemas/cards_creatures.json", "r" ,encoding="utf-8") as file:
+SCHEMA_FILE = Path(__file__).parent.parent / "src" / "database" / "schemas" / "cards_creatures.json"
+
+with open(SCHEMA_FILE, "r", encoding="utf-8") as file:
     data = json.load(file)
 
     texts = build_card_text(data)
